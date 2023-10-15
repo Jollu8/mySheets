3e1512bce7088877fb3a9f26af9e219c8cfa3c56
Jollu8 <zholu8@gmail.com>
2023-10-15 14:44:04 +0300

    !

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+#include <string>
+#include <string_view>
+
+#include "src/header/common.h"
+#include "src/header/test_runner_p.h"
+
+inline std::ostream& operator<<(std::ostream& output, Position pos) {
+    return output << "(" << pos.row << ", " << pos.col << ")";
+}
+
+inline Position operator"" _pos(const char* str, std::size_t) {
+    return Position::FromString(str);
+}
+
+int main() {
+
+    return 0;
+}
--- src/formula.cpp
+++ src/formula.cpp
@@ -39,4 +39,54 @@ struct CellValueGetter {
     double operator()(const FormulaError &error) {
         throw error;
     }
+};
+
+namespace {
+    class Formula : public FormulaInterface {
+    public: // Constructor
+        explicit Formula(const std::string &expression) : ast_(ParseFormulaAST(expression)),
+                                                          referenced_cells_(ast_.GetCells().begin(),
+                                                                            ast_.GetCells().end()) {
+            auto last = std::unique(referenced_cells_.begin(), referenced_cells_.end());
+            referenced_cells_.erase(last, referenced_cells_.end());
+            std::sort(referenced_cells_.begin(), referenced_cells_.end());
+        }
+
+    public: // Methods
+        Value Evaluate(const SheetInterface &sheet) const override {
+            try {
+                auto get_value = [&sheet](const Position &position) {
+                    if (auto *cell = sheet.GetCell(position)) return std::visit(CellValueGetter{}, cell->GetValue());
+                    return 0.;
+                };
+                return ast_.Execute(get_value);
+
+            } catch (const FormulaError &error_code) {
+                return error_code;
+            }
+        }
+
+        std::string GetExpression() const override {
+            std::stringstream ss;
+            ast_.PrintFormula(ss);
+            return ss.str();
+        }
+
+        std::vector<Position> GetReferencedCells() const override {
+            return referenced_cells_;
+        }
+
+    private: // Fields
+        FormulaAST ast_;
+        std::vector<Position> referenced_cells_;
+    };
+}
+
+std::unique_ptr<FormulaInterface> ParseFormula(const std::string &expression) {
+    try {
+        return std::make_unique<Formula>(expression);
+
+    } catch (std::exception &e) {
+        throw FormulaException(e.what());
+    }
 }
